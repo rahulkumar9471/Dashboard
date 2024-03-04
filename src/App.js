0e6b8a6ca26cf3bafb3258d82dc69a10cf7ed5bf
Rahul Kumar <rk8717989@gmail.com>
2024-03-04 15:46:21 +0530

    theme cookie add

--- src/App.js
+++ src/App.js
@@ -3,40 +3,45 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Dashboard from './Pages/Dashboard';
-import { useEffect, useState } from 'react';
 import Setting from './Pages/Setting';
+import { useEffect, useState } from 'react';
+import { CookiesProvider, useCookies } from 'react-cookie';
 
 function App() {
+  const [isSidebar, setIsSidebar] = useState(true);
+  const [cookies, setCookie] = useCookies(['theme']); // Ensure you provide the cookie name here
 
-  const [isSIdebar, setIsSidebar] = useState(true);
+  // Initialize theme state with the value from cookies, or default to 'light'
+  const [theme, setTheme] = useState(cookies.theme || 'light');
 
   const toggleSidebar = () => {
-    setIsSidebar(!isSIdebar);
+    setIsSidebar(!isSidebar);
   }
 
-  const [theme, setTheme] = useState("light");
-
   const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    // Toggle the theme and set the cookie
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    setCookie('theme', newTheme, { path: '/' });
   }
 
   useEffect(() => {
     document.body.className = theme;
-  },[theme])
+  }, [theme])
 
   return (
-    <>
-      <div className={`${theme}`}>
-        <Navbar toggleSidebar={toggleSidebar} toggleTheme={toggleTheme} theme={theme}/>
+    <CookiesProvider>
+      <div className={theme}>
+        <Navbar toggleSidebar={toggleSidebar} toggleTheme={toggleTheme} theme={theme} />
         <div className="flex justify-start w-full">
-          <Sidebar isSIdebar={isSIdebar} theme={theme}/>
+          <Sidebar isSIdebar={isSidebar} theme={theme} />
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/profile/settings' element={<Setting />} />
           </Routes>
         </div>
       </div>
-    </>
+    </CookiesProvider>
   );
 }
 
